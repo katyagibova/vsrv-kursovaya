1f431e97d243cee706abaf2bad004e628b9e2a86
Ekaterina Gibova <katyagibova@bk.ru>
2021-12-08 04:04:46 +0300

    started doing the algorithm of the second cut

--- src/components/Manufactory.vue
+++ src/components/Manufactory.vue
@@ -12,7 +12,7 @@
     </v-row>
     <v-row>
       <v-col align-self="center" cols="3">
-        <p class="text">В очереди: <b>{{count}}</b> хлыстов</p>
+        <p class="text">В очереди: <b>{{countH}}</b> хлыстов</p>
       </v-col>
       <v-col cols="3">
         <v-progress-linear
@@ -39,18 +39,18 @@
       </v-col>
       <v-col cols="3" align-self="center">
         <p class="text">Получено:</p>
-        <p class="text">бруса - <b>{{count}}</b><v-icon color="#1E1918">mdi-arrow-right-thin</v-icon> </p>
-        <p class="text">горбылей - <b>{{count}}</b><v-icon color="#1E1918">mdi-arrow-down-thin</v-icon> </p>
+        <p class="text">бруса - <b>{{countB}}</b><v-icon color="#1E1918">mdi-arrow-right-thin</v-icon> </p>
+        <p class="text">горбылей - <b>{{countH}}</b><v-icon color="#1E1918">mdi-arrow-down-thin</v-icon> </p>
       </v-col>
       <v-col cols="3">
         <v-progress-linear
         color="#B64D2F"
         buffer-value="0"
         height="17px"
-        :value="progress"
+        :value="progressB"
         stream
         >
-          <strong>{{ Math.ceil(progress) }}%</strong>
+          <strong>{{ Math.ceil(progressB) }}%</strong>
         </v-progress-linear>
         <v-img contain src="@/assets/secondM.png" alt="Krafter M" max-height="60px">
         </v-img>
@@ -79,7 +79,7 @@
       </v-col>
     </v-row>
     <v-dialog
-      v-model="addNewH"
+      v-model="addNewHDialog"
       max-width="600px"
     >
       <v-card>
@@ -119,14 +119,14 @@
           <v-btn
             color="blue darken-1"
             text
-            @click="addNewH = false"
+            @click="addNewHDialog = false"
           >
             Отмена
           </v-btn>
           <v-btn
             color="blue darken-1"
             text
-            @click="addNewObject"
+            @click="addNewH"
           >
             Добавить
           </v-btn>
@@ -141,44 +141,65 @@
     name: 'Manufactory',
     data () {
       return{
-        addNewH: false,
+        addNewHDialog: false,
         errorDialog: false,
         diameter: 0,
         boolDiameter: 0,
         curvature: 0,
         boolCurvature: 0,
         allH: [],
+        allB: [],
         index: 0,
+        indexB: 0,
         hQueue: 0,
         progress: 0,
+        progressB: 0,
         interval: 0,
-        count: 0,
+        intervalB: 0,
+        countH: 0,
+        countB: 0,
         i: 0,
+        iB: 0,
       }
     },
     watch: {
       progress (val) {
         if (val <= 100) return
+
         this.firstWorking()
+      },
+
+      progressB (val) {
+        if (val <= 100) return
+
+        this.secondMWorking()
       },
     },
     methods:{
       activate() {
         this.interval = setInterval(() => {
-          console.log("я работаю")
           this.progress = this.progress + 10
         }, 2000)
-        
+      },
+
+      activateB() {
+        this.intervalB = setInterval(() => {
+          this.progressB = this.progressB + 10
+        }, 2000)
       },
 
       openHDialog(){
-        this.addNewH = true
-        
+        this.diameter = 0
+        this.curvature = 0
+        this.addNewHDialog = true
       },
 
       firstWorking(){
         this.i++
-        if(this.count == 0){
+        //this.addNewB()
+        console.log("Надеюсь тут заканчивается распил 1 хлыста")
+        console.log(this.i)
+        if(this.countH == 0){
           this.progress = 0
           setTimeout(() => { clearInterval(this.interval) }, 0);
           this.index = 0
@@ -189,13 +210,31 @@
           if(this.i === 1){
             this.activate()
           }
-          this.count--
+          this.countH--
           return true
         }
       },
 
-      addNewObject(){
-        
+      secondMWorking(){
+        this.iB++
+        console.log("Надеюсь тут заканчивается распил 1 бруса")
+        if(this.countB == 0){
+          this.progressB = 0
+          setTimeout(() => { clearInterval(this.intervalB) }, 0);
+          this.indexB = 0
+          this.allB = []
+          return true
+        } else {
+          this.progressB = 0
+          if(this.iB === 1){
+            this.activateB()
+          }
+          this.countB--
+          return true
+        }
+      },
+
+      addNewH(){
         if(this.diameter >= 12 && this.diameter <= 46){
           this.boolDiameter = true
         } else {
@@ -216,20 +255,44 @@
         this.allH.push(newObject)
 
         if(this.allH.length == 1){
-          this.count = this.allH.length
+          this.countH = this.allH.length
         } else {
-          this.count = this.allH.length - 1
+          this.countH = this.allH.length - 1
         }
 
         console.log(this.allH)
-        this.addNewH = false
+        this.addNewHDialog = false
         this.index++
         if(this.index === 1){
           this.interval = 0
           this.i = 0
           this.firstWorking()
         }
-        console.log(this.count)
+        console.log(this.countH)
+      },
+
+      addNewB(){
+        // for(let b = 0; b < 2; b++){
+        let newObject = {
+          error: false,
+        }
+        this.allB.push(newObject)
+        // }
+        
+
+        if(this.allB.length == 1){
+          this.countB = this.allB.length
+        } else {
+          this.countB = this.allB.length - 1
+        }
+
+        //console.log(this.allB)
+        this.indexB++
+        if(this.index === 1){
+          this.intervalB = 0
+          this.iB = 0
+          this.secondMWorking()
+        }
       }
     },
     created(){
